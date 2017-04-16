f7806c4cebc9269b007ed15fc83f35dc47b441bf
advanderveer <advanderveer@gmail.com>
2017-04-16 17:37:07 +0200

    added a gateway proxy handler

--- examples/hello_world/main.go
+++ examples/hello_world/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"regexp"
 
 	"github.com/microfactory/line"
-	"github.com/microfactory/line/session"
 )
 
-var gatewayHandler = line.HandlerFunc(
-	func(ctx context.Context, msg json.RawMessage) (interface{}, error) {
-		return "hello gateway", nil
-	})
+var gatewayHandler = line.NewGatewayHandler(0, http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `{"hello": "world"}`)
+	}))
 
 var scheduleHandler = line.HandlerFunc(
 	func(ctx context.Context, msg json.RawMessage) (interface{}, error) {
@@ -26,7 +27,7 @@ func Handle(msg json.RawMessage, invoc *line.Invocation) (interface{}, error) {
 	mux.MatchARN(regexp.MustCompile(`-schedule$`), gatewayHandler)
 
 	mux.Use(line.EarlyTimeout(5000)) //gives handlers 5sec to clean up
-	mux.Use(session.LineSession())   //add an aws session to the context
+	mux.Use(line.WithSession())      //adds an aws session to the context
 
 	return mux.Handle(msg, invoc)
 }
--- middleware.go
+++ middleware.go
@@ -3,7 +3,12 @@ package line
 import (
 	"context"
 	"encoding/json"
+	"os"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 //EarlyTimeout will cancel the lambda context millisecond before actual timeout to give handlers time to shutdown cleanly
@@ -18,6 +23,50 @@ func EarlyTimeout(shutdownTimeMillis int64) func(Handler) Handler {
 
 				ctx, cancel := context.WithTimeout(ctx, time.Millisecond*time.Duration(lctx.RemainingTimeInMillis()-5000))
 				defer cancel()
+				return h.HandleEvent(ctx, msg)
+			})
+	}
+}
+
+//FromContext will return a aws session from the lambda context or panic if it isn't available
+func FromContext(ctx context.Context) *session.Session {
+	sess, ok := ctx.Value(sessionKey).(*session.Session)
+	if !ok {
+		panic("no aws session available in context")
+	}
+
+	return sess
+}
+
+const sessionKey key = 0
+
+//WithSession will include a aws.Session into the context with line specific environment
+func WithSession() func(Handler) Handler {
+	region := os.Getenv("LINE_AWS_REGION")
+	accessKey := os.Getenv("LINE_AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("LINE_AWS_SECRET_ACCESS_KEY")
+	if region == "" || accessKey == "" || secretKey == "" {
+		panic("cannot use line session without all of the following environment variables: LINE_AWS_REGION, LINE_AWS_ACCESS_KEY_ID, LINE_AWS_SECRET_ACCESS_KEY")
+	}
+
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String(region),
+			Credentials: credentials.NewStaticCredentials(
+				accessKey,
+				secretKey,
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic("failed to setup AWS session: " + err.Error())
+	}
+
+	return func(h Handler) Handler {
+		return HandlerFunc(
+			func(ctx context.Context, msg json.RawMessage) (interface{}, error) {
+				ctx = context.WithValue(ctx, sessionKey, sess)
 				return h.HandleEvent(ctx, msg)
 			})
 	}
